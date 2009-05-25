deaca7b88c88b10084f8c70b2cb8ed8ff26d7702
ninjapanzer <ninjapanzer@1059ad3b-92a6-4bf0-9bf7-c212266dadba>
2009-05-25 03:55:23 +0000

    git-svn-id: https://autokindle.svn.sourceforge.net/svnroot/autokindle@2 1059ad3b-92a6-4bf0-9bf7-c212266dadba

---
+++ C#/AutoKindle Updater/AutoKindle Updater/Ftp.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Net;
+using System.Collections.Generic;
+using System.IO;
+using System.Windows;
+using Global_Tools;
+
+namespace AutoKindle_Updater
+{
+    class Ftp
+    {
+        public struct ftpCredentials
+        {
+            public string filePath;
+            public string fileName;
+            public string ftpServerIP;
+            public string ftpUserID;
+            public string ftpPassword;
+        }
+
+        private Queue<string> fileNameList = new Queue<string>();
+
+        public ftpCredentials ftpCred = new ftpCredentials();
+	    public Ftp()
+	    {
+            ftpCred.fileName = "";
+            ftpCred.filePath = "";
+            ftpCred.ftpServerIP = "";
+            ftpCred.ftpUserID = "";
+            ftpCred.ftpPassword = "";
+	    }
+        public Ftp(string filePath, string fileName, string ftpServerIP, string ftpUserID, string ftpPassword)
+        {
+            //ftpCredentials ftpCred = new ftpCredentials();
+            ftpCred.filePath = filePath;
+            ftpCred.fileName = fileName;
+            ftpCred.ftpServerIP = ftpServerIP;
+            ftpCred.ftpUserID = ftpUserID;
+            ftpCred.ftpPassword = ftpPassword;
+            getDirListFtp();
+            getFileListFtp();
+        }
+
+
+        public Queue<string> getFileListQueue()
+        {
+            return this.fileNameList;
+        }
+        public static string dnsResolve(string ftpServerName)
+        {
+            string ftpServerIP;
+            IPHostEntry ServerIP = Dns.GetHostEntry(ftpServerName);
+            ftpServerIP = ServerIP.AddressList[0].ToString();
+            return ftpServerIP;
+        }
+        public void getFileFtp()
+        {
+            FtpWebRequest reqFTP;
+            try
+            {
+                //filePath: The full path where the file is to be created.
+                //fileName: Name of the file to be createdNeed not name on
+                //          the FTP server. name name()
+                FileStream outputStream = new FileStream(this.ftpCred.filePath + "\\" + this.ftpCred.fileName, FileMode.Create);
+                reqFTP = (FtpWebRequest)FtpWebRequest.Create(new Uri("ftp://" + this.ftpCred.ftpServerIP + "/" + this.ftpCred.fileName));
+                reqFTP.Method = WebRequestMethods.Ftp.DownloadFile;
+                reqFTP.UseBinary = true;
+                reqFTP.Credentials = new NetworkCredential(this.ftpCred.ftpUserID, this.ftpCred.ftpPassword);
+                FtpWebResponse response = (FtpWebResponse)reqFTP.GetResponse();
+                Stream ftpStream = response.GetResponseStream();
+                long cl = response.ContentLength;
+                int bufferSize = 2048;
+                int readCount;
+                byte[] buffer = new byte[bufferSize];
+
+                readCount = ftpStream.Read(buffer, 0, bufferSize);
+                while (readCount > 0)
+                {
+                    outputStream.Write(buffer, 0, readCount);
+                    readCount = ftpStream.Read(buffer, 0, bufferSize);
+                }
+
+                ftpStream.Close();
+                outputStream.Close();
+                response.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+        public void getFileListFtp()
+        {
+            this.ftpCred.fileName = "currentlist.pnz";
+            this.ftpCred.filePath = GlobalTools.GetexeLocation();
+            this.getFileFtp();
+            if (GlobalTools.FileExist(this.ftpCred.filePath + "//" + this.ftpCred.fileName))
+            {
+                string listInput;
+                TextReader ReadText = new StreamReader(this.ftpCred.filePath + "//" + this.ftpCred.fileName);
+                while ((listInput = ReadText.ReadLine()) != null)
+                {
+                    if (listInput != "FileList")
+                    {
+                        this.fileNameList.Enqueue(listInput);
+                    }
+                }
+            }
+        }
+        public void getDirListFtp()
+        {
+            this.ftpCred.fileName = "directories.pnz";
+            this.ftpCred.filePath = GlobalTools.GetexeLocation();
+            this.getFileFtp();
+            if (GlobalTools.FileExist(this.ftpCred.filePath + "//" + this.ftpCred.fileName))
+            {
+                string dirInput;
+                TextReader ReadText = new StreamReader(this.ftpCred.filePath + "//" + this.ftpCred.fileName);
+                while ((dirInput = ReadText.ReadLine()) != null)
+                {
+                    if (dirInput != "FileList")
+                    {
+                        Directory.CreateDirectory(this.ftpCred.filePath + "//" + dirInput);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ C#/AutoKindle Updater/AutoKindle Updater/Window1.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Net;
+using System.IO;
+using Global_Tools;
+
+namespace AutoKindle_Updater
+{
+    /// <summary>
+    /// Interaction logic for Window1.xaml
+    /// </summary>
+    public partial class Window1 : Window
+    {
+        public Window1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            Ftp ftpaccess = new Ftp(GlobalTools.GetexeLocation(), "simple.pnz", Ftp.dnsResolve("ftp.technosamurai.net"), "public@technosamurai.net", "public");
+            StatusTxt.AppendText("Creating Missing Folders\n");
+            Queue<string> fileNameList = ftpaccess.getFileListQueue();
+            ftpaccess.ftpCred.filePath = GlobalTools.GetexeLocation();
+            foreach (string fileName in fileNameList)
+            {
+                //if (GlobalTools.FileExist(GlobalTools.GetexeLocation() + fileName))
+                //{
+                ftpaccess.ftpCred.fileName = fileName;
+                StatusTxt.AppendText("Getting " + fileName + "......");
+                ftpaccess.getFileFtp();
+                StatusTxt.AppendText("Finished\n");
+                //}
+            }
+        }
+    }
+}
